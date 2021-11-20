a14c27923a71cb2ad2e312a3668ae1d5a61e0acb
Quinten <quinten.roets@gmail.com>
2021-11-20 23:11:23 +0100

    first commit

---
+++ colortheme/starter.py
@@ -0,0 +1,92 @@
+import geocoder
+import astral
+from astral.sun import sun
+from datetime import datetime
+import time
+import getpass
+
+from libs.cli import Cli
+from datetime import datetime
+
+from .filemanager import FileManager
+
+def main():
+    while True:
+        check_theme()
+
+def check_theme():
+    location = geocoder.ip("me").current_result.raw
+    location_info = astral.LocationInfo(location["city"], location["region"], location["country"])
+    sun_info = sun(location_info.observer)
+    
+    dawn = sun_info["dawn"].replace(tzinfo=None)    
+    dusk = sun_info["dusk"].replace(tzinfo=None)
+        
+    settings = FileManager.load("settings")
+    daytime = settings
+        
+    while daytime == settings:
+        now = datetime.now()
+        daytime = dawn < now and now < dusk
+        daytime = "light" if daytime else "dark"
+        if daytime == settings:
+            time.sleep(5)
+    
+    if daytime == "light":
+        go_light()
+    elif daytime == "dark":
+        go_dark()
+    
+def go_light():
+    change_colortheme("dark", "light")
+    
+def go_dark():
+    change_colortheme("light", "dark")
+    
+def change_colortheme(old, new):
+    programs = ["chromium", "pycharm", "dolphin", "kate"]
+    open_programs = {p: Cli.get(f"xdotool search --onlyvisible {p}", check=False) for p in programs}
+    
+    if open_programs["chromium"]:
+        close("chromium")
+        
+    Cli.get(
+        f"konsave -f -s {old}",
+        f"konsave -a $(konsave -l | grep {new} | cut -f1)"
+    )
+    
+    time.sleep(2)
+        
+    for program, opened in open_programs.items():
+        if opened and program != "chromium":
+            close(program)
+            Cli.run(program, wait=False)
+    
+    restartplasma()
+    FileManager.save(new, "settings")
+    
+def close(name):
+    Cli.get(f"xdotool search --onlyvisible {name} | xargs -i% wmctrl -i -c %")
+    
+def restartplasma():
+    Cli.run(
+        "plasmashell --replace",
+        "kwin --replace",
+        "systemctl --user restart pulseaudio",
+        wait=False
+        )
+    
+def save_light():
+    save_theme("light")
+    
+def save_dark():
+    save_theme("dark")
+    
+def save_theme(name):
+    Cli.run(
+        f"konsave -f -s {name}",
+        "konsave -e $(konsave -l | grep light | cut -f1)"
+        )
+ 
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from libs.cli import Cli
+
+NAME = "colortheme"
+
+def read(filename):
+    try:
+        with open(filename) as fp:
+            content = fp.read().split("\n")
+    except FileNotFoundError:
+        content = []
+    return content
+
+
+setup(
+    author="Quinten Roets",
+    author_email="quinten.roets@gmail.com",
+    description='',
+    name=NAME,
+    version='1.0',
+    packages=find_packages(),
+    install_requires=read("requirements.txt"),
+    entry_points={
+        "console_scripts": [
+            "colortheme = colortheme.starter:main",
+            "dark = colortheme.starter:go_dark",
+            "light = colortheme.starter:go_light",
+            "savelight = colortheme.starter:save_light",
+            "savedark = colortheme.starter:save_dark",
+            "restartplasma = colortheme.starter:restartplasma",
+            ]
+        },
+)
+
+Cli.install(read("packages.txt"))
