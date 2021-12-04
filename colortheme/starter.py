ba7a3913ede5b4c7a15621898e8f21c09facd105
Quinten <quinten.roets@gmail.com>
2021-12-05 00:33:16 +0100

    refactor colortheme

--- colortheme/starter.py
+++ colortheme/starter.py
@@ -1,136 +1,29 @@
-import geocoder
-import astral
-from astral.sun import sun
-from datetime import datetime
-import time
-import getpass
+import sys
 
-from libs.cli import Cli
-from datetime import datetime, timedelta
-
-from .filemanager import FileManager
+from .thememanager import ThemeManager
 
 def main():
-    while True:
-        check_theme()
+    args = sys.argv[1:]
+    for theme in ["light", "dark"]:
+        if theme in args:
+            ThemeManager.change_colortheme(theme)
+        elif f"save{theme}" in args:
+            ThemeManager.save_theme(theme)
 
-def check_theme():
-    dawn, dusk = get_sun_events()
-    settings = get_theme()
-    daytime = settings
-            
-    while daytime == settings:
-        now = datetime.now()
-        if now.date() > dusk.date():
-            # recalculate event if already next day
-            dawn, dusk = get_sun_events()
-        
-        daytime = "light" if dawn < now < dusk else "dark"
-        if daytime == settings:
-            time.sleep(5)
-    
-    if daytime == "light":
-        go_light(confirm=True)
-    elif daytime == "dark":
-        go_dark(confirm=True)
-        
-def get_sun_events():
-    result = geocoder.ip("me").current_result
-    # fallback location when no internet
-    location = result.raw if result else {'city': 'Brugge', 'region': 'Flanders', 'country': 'BE'}
-    location_info = astral.LocationInfo(location["city"], location["region"], location["country"])
-    
-    dawn, dusk = get_sun_info(location_info)
-    now = datetime.now()
-    if now > dusk:
-        # get events for next day if already dark
-        dawn, dusk = get_sun_info(location_info, date=now+timedelta(days=1))
-    return dawn, dusk
-    
+def go_dark():
+    ThemeManager.change_colortheme("dark")
 
-def get_sun_info(location_info, date=None):
-    sun_info = sun(location_info.observer, date=date)
-    
-    dawn = sun_info["dawn"].replace(tzinfo=None) - timedelta(minutes=30) # light = dawn - 30 min
-    dusk = sun_info["dusk"].replace(tzinfo=None) + timedelta(minutes=30) # dark = dusk + 30 min
-    
-    return dawn, dusk
-    
-def go_light(confirm=False):
-    if get_theme() == "dark":
-        change_colortheme("dark", "light", confirm=confirm)
-    
-def go_dark(confirm=False):
-    if get_theme() == "light":
-        change_colortheme("light", "dark", confirm=confirm)
-    
-def change_colortheme(old, new, confirm=False):
-    programs = ["chromium", "pycharm", "dolphin", "kate"]
-    open_programs = {p: Cli.get(f"xdotool search --onlyvisible {p}", check=False) for p in programs}
-    
-    ask_confirmation = confirm and any(open_programs.values())
-    if ask_confirmation:
-        try:
-            Cli.get(f"kdialog --yesno 'Change to {new} theme?'")
-        except:
-            now = datetime.now()
-            dawn, dusk = get_sun_events()
-            next_event = dawn if now < dawn else dusk
-            while now < next_event:
-                time.sleep(5)
-                now = datetime.now()
-            return
-            # dont change colortheme if not confirmed
-            # and dont ask again until next event
-    
-    if open_programs["chromium"]:
-        close("chromium")
+def go_light():
+    ThemeManager.change_colortheme("light")
 
-    change_config(old, new)
-            
-    for program, opened in open_programs.items():
-        if opened:
-            if program != "chromium":
-                close(program)
-                sleep_time = 2 if program == "pycharm" else 0.5
-                time.sleep(sleep_time)
-            Cli.run(program, wait=False)
-    
-    restartplasma()
-    FileManager.save(new, "settings")
-
-def change_config(old, new):
-    Cli.get(
-        f"konsave -f -s {old}",
-        f"konsave -a $(konsave -l | grep {new} | cut -f1)"
-    )
-    
-def close(name):
-    Cli.get(f"xdotool search --onlyvisible {name} | xargs -i% wmctrl -i -c %")
-    
-def restartplasma():
-    Cli.run(
-        "plasmashell --replace",
-        "kwin --replace",
-        wait=False
-        )
-    
 def save_light():
-    if get_theme() == "light":
-        save_theme("light")
-    
+    ThemeManager.save_theme("light")
+
 def save_dark():
-    if get_theme() == "dark":
-        save_theme("dark")
-    
-def save_theme(name):
-    Cli.run(
-        f"konsave -f -s {name}",
-        f"konsave -e $(konsave -l | grep {name} | cut -f1)"
-        )
-    
-def get_theme():
-    return FileManager.load("settings")
- 
+    ThemeManager.save_theme("dark")
+
+def restartplasma():
+    ThemeManager.restartplasma()
+
 if __name__ == "__main__":
     main()
---
+++ colortheme/thememanager.py
@@ -0,0 +1,131 @@
+import geocoder
+import astral
+from astral.sun import sun
+from datetime import datetime
+import time
+import getpass
+
+from libs.cli import Cli
+from libs.gui import Gui
+from libs.threading import Threads
+from datetime import datetime, timedelta
+
+from .filemanager import FileManager
+
+class ThemeManager:
+    @staticmethod
+    def check_theme():
+        dawn, dusk = ThemeManager.get_sun_events()
+        settings = ThemeManager.get_theme()
+        daytime = settings
+
+        while daytime == settings:
+            now = datetime.now()
+            if now.date() > dusk.date():
+                # recalculate event if already next day
+                dawn, dusk = ThemeManager.get_sun_events()
+
+            daytime = "light" if dawn < now < dusk else "dark"
+            if daytime == settings:
+                time.sleep(5)
+
+        ThemeManager.change_colortheme(daytime, confirm=True)
+
+    @staticmethod
+    def get_sun_events():
+        result = geocoder.ip("me").current_result
+        # fallback location when no internet
+        location = result.raw if result else {'city': 'Brugge', 'region': 'Flanders', 'country': 'BE'}
+        location_info = astral.LocationInfo(location["city"], location["region"], location["country"])
+
+        dawn, dusk = ThemeManager.get_sun_info(location_info)
+        now = datetime.now()
+        if now > dusk:
+            # get events for next day if already dark
+            dawn, dusk = ThemeManager.get_sun_info(location_info, date=now+timedelta(days=1))
+        return dawn, dusk
+    
+    @staticmethod
+    def get_sun_info(location_info, date=None):
+        sun_info = sun(location_info.observer, date=date)
+
+        dawn = sun_info["dawn"].replace(tzinfo=None) - timedelta(minutes=30) # light = dawn - 30 min
+        dusk = sun_info["dusk"].replace(tzinfo=None) + timedelta(minutes=30) # dark = dusk + 30 min
+
+        return dawn, dusk
+
+    @staticmethod
+    def change_colortheme(new, confirm=False):
+        old = "dark" if new == "light" else "light"
+        if ThemeManager.get_theme() == old:
+            programs = ["chromium", "pycharm", "dolphin", "kate"]
+            if confirm:
+                programs.append("konsole")
+            open_programs = [p for p in programs if Cli.get(f"xdotool search --onlyvisible {p}", check=False)]
+            canceled = (
+                confirm and
+                any(open_programs) and
+                not Gui.ask_yn(f"Change to {new} theme?")
+            )
+            if canceled:
+                now = datetime.now()
+                dawn, dusk = get_sun_events()
+                next_event = dawn if now < dawn else dusk
+                while now < next_event:
+                    time.sleep(5)
+                    now = datetime.now()
+                # dont change colortheme if not confirmed
+                # and dont ask again until next event
+            else:
+                ThemeManager.start_colortheme_change(old, new, open_programs)
+
+    @staticmethod
+    def start_colortheme_change(old, new, open_programs):
+        if "chromium" in open_programs:
+            ThemeManager.close("chromium")
+
+        ThemeManager.change_config(old, new)
+        ThemeManager.restartplasma()
+        Threads(ThemeManager.close, open_programs).join()
+
+        if "pycharm" in open_programs:
+            time.sleep(1.5)
+            open_programs.remove("pycharm")
+            open_programs.append("pycharm-professional")
+
+        Threads(Cli.run, open_programs, wait=False).join()
+        FileManager.save(new, "settings")
+
+    @staticmethod
+    def change_config(old, new):
+        Cli.get(
+            f"konsave -f -s {old}",
+            f"konsave -a $(konsave -l | grep {new} | cut -f1)"
+        )
+
+    @staticmethod
+    def close(name):
+        Cli.get(f"xdotool search --onlyvisible {name} | xargs -i% wmctrl -i -c %")
+        time.sleep(0.5)
+        # wait until closed
+        while Cli.get(f"xdotool search --onlyvisible {name}", check=False):
+            time.sleep(0.5)
+
+    @staticmethod
+    def restartplasma():
+        Cli.run("plasmashell --replace", "kwin --replace", wait=False)
+
+    @staticmethod
+    def save_theme(name):
+        if ThemeManager.get_theme() == name:
+            Cli.run(
+                f"konsave -f -s {name}",
+                f"konsave -e $(konsave -l | grep {name} | cut -f1)"
+                )
+
+    @staticmethod
+    def get_theme():
+        return FileManager.load("settings")
+ 
+if __name__ == "__main__":
+    main()
