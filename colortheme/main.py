222803adfa44bc5e058ca9d36ec8c310a9b0b8bb
Quinten <quinten.roets@gmail.com>
2022-01-16 16:38:59 +0100

    use single quotes

--- colortheme/main.py
+++ colortheme/main.py
@@ -11,30 +11,30 @@ def main(action=None):
 
 def _main(action=None):
     parser = argparse.ArgumentParser(description='Colortheme actions')
-    parser.add_argument('action', nargs='?', help='The action to do [check[default], dark, light, restart]', default="check")
+    parser.add_argument('action', nargs='?', help='The action to do [check[default], dark, light, restart]', default='check')
     args = parser.parse_args()
     if action is not None:
         args.action = action
     
-    if args.action == "check":
+    if args.action == 'check':
         ThemeManager.start_check_service()
-    elif args.action == "restart":
+    elif args.action == 'restart':
         ThemeManager.restartplasma()
     else:
         ThemeManager.apply(args.action)
 
 
 def go_dark():
-    main("dark")
+    main('dark')
 
 
 def go_light():
-    main("light")
+    main('light')
 
 
 def restartplasma():
-    main("restart")
+    main('restart')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
