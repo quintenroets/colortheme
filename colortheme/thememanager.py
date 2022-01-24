ef7ec0a6b14c99cc81e23efa0ca8e86569837c84
Quinten <quinten.roets@gmail.com>
2022-01-24 15:44:19 +0100

    autoformat and sort imports

--- colortheme/thememanager.py
+++ colortheme/thememanager.py
@@ -1,14 +1,13 @@
-import astral
-import cli
-import geocoder
-import gui
 import time
-
-from astral.sun import sun
 from datetime import datetime, timedelta
 
-from backup.profilemanager import ProfileManager
+import astral
+import geocoder
+from astral.sun import sun
 
+import cli
+import gui
+from backup.profilemanager import ProfileManager
 from libs.threading import Threads
 
 
@@ -23,15 +22,15 @@ class ThemeManager:
     def start_check_service():
         while True:
             ThemeManager.check_theme()
-    
+
     @staticmethod
     def check_theme():
         now = datetime.now()
         sunhours = ThemeManager.sun_hours
-        daytime = 'light' if sunhours.dawn < now < sunhours.dusk else 'dark'
+        daytime = "light" if sunhours.dawn < now < sunhours.dusk else "dark"
         if daytime != ProfileManager.active_profile:
             ThemeManager.apply(daytime, ask_confirm=True)
-        
+
         next_event = sunhours.dawn if now < sunhours.dawn else sunhours.dusk
         while datetime.now() < next_event:
             time.sleep(5)
@@ -39,34 +38,50 @@ class ThemeManager:
     @classmethod
     @property
     def sun_hours(cls):
-        result = geocoder.ip('me').current_result
+        result = geocoder.ip("me").current_result
         # fallback location when no internet or too many requests
-        location = result.raw if result else {'city': 'Brugge', 'region': 'Flanders', 'country': 'BE'}        
-        location_info = astral.LocationInfo(location['city'], location['region'], location['country'])
+        location = (
+            result.raw
+            if result
+            else {"city": "Brugge", "region": "Flanders", "country": "BE"}
+        )
+        location_info = astral.LocationInfo(
+            location["city"], location["region"], location["country"]
+        )
 
         sunhours = ThemeManager.get_sun_info(location_info)
         now = datetime.now()
         if now > sunhours.dusk:  # get events for next day if already dark
-            sunhours = ThemeManager.get_sun_info(location_info, date=now+timedelta(days=1))
+            sunhours = ThemeManager.get_sun_info(
+                location_info, date=now + timedelta(days=1)
+            )
         return sunhours
-    
+
     @staticmethod
     def get_sun_info(location_info, date=None):
         sun_info = sun(location_info.observer, date=date)
 
-        dawn = sun_info['dawn'].replace(tzinfo=None) - timedelta(minutes=30) # light = dawn - 30 min
-        dusk = sun_info['dusk'].replace(tzinfo=None) + timedelta(minutes=30) # dark = dusk + 30 min
-        
+        dawn = sun_info["dawn"].replace(tzinfo=None) - timedelta(
+            minutes=30
+        )  # light = dawn - 30 min
+        dusk = sun_info["dusk"].replace(tzinfo=None) + timedelta(
+            minutes=30
+        )  # dark = dusk + 30 min
+
         return SunHours(dawn, dusk)
 
     @staticmethod
     def apply(name, ask_confirm=False):
-        programs = ['pycharm', 'dolphin', 'kate', 'chromium']
-        open_programs = [p for p in programs if cli.get('xdotool search --onlyvisible', p, check=False)]
+        programs = ["pycharm", "dolphin", "kate", "chromium"]
+        open_programs = [
+            p
+            for p in programs
+            if cli.get("xdotool search --onlyvisible", p, check=False)
+        ]
         confirmed = (
             not ask_confirm
             or not any(open_programs)
-            or gui.ask_yn(f'Change to {name} theme?')
+            or gui.ask_yn(f"Change to {name} theme?")
         )
         if confirmed:
             ThemeManager.start_apply(name, open_programs)
@@ -76,9 +91,9 @@ class ThemeManager:
     @staticmethod
     def start_apply(name, open_programs):
         custom_apply_mapping = {
-            'chromium': ThemeManager.apply_chromium,
-            'pycharm': ThemeManager.apply_pycharm
-            }
+            "chromium": ThemeManager.apply_chromium,
+            "pycharm": ThemeManager.apply_pycharm,
+        }
         custom_apply = set({})
         for name, function in custom_apply_mapping.items():
             if name in open_programs:
@@ -87,46 +102,46 @@ class ThemeManager:
 
         ProfileManager.apply(name)
         ThemeManager.restartplasma()
-        Threads(ThemeManager.restart, args=(open_programs, )).start().join()
-        
+        Threads(ThemeManager.restart, args=(open_programs,)).start().join()
+
         for custom in custom_apply:
             time.sleep(2)
             custom()
 
     @staticmethod
     def restart(name):
-        cli.get(f'wmctrl -c {name}')
-        
+        cli.get(f"wmctrl -c {name}")
+
         # wait until closed
-        while cli.get('xdotool search --onlyvisible', name, check=False):
+        while cli.get("xdotool search --onlyvisible", name, check=False):
             time.sleep(0.5)
-        
+
         cli.start(name)
 
     @staticmethod
     def restartplasma():
-        cli.run_commands('plasmashell --replace', 'kwin --replace', wait=False)
+        cli.run_commands("plasmashell --replace", "kwin --replace", wait=False)
 
     @staticmethod
     def apply_pycharm(name):
-        letter = 'd' if name == 'dark' else 'i'
+        letter = "d" if name == "dark" else "i"
         cli.run_commands(
-            'jumpapp pycharm',
-            f'xdotool key --clearmodifiers ctrl+shift+alt+y t Return {letter} Return'
+            "jumpapp pycharm",
+            f"xdotool key --clearmodifiers ctrl+shift+alt+y t Return {letter} Return",
         )
 
     @staticmethod
     def apply_chromium(name):
-        direction = 'Left' if name == 'light' else 'Right'
-        cli.run_commands(            
-            'jumpapp chromium',
-            'sleep 1',
-            'xdotool key ctrl+t',
-            'xdotool type chrome://flags',
-            'xdotool key Return',
-            'sleep 0.5', 
-            'xdotool type dark',
-            'xdotool key Return',
-            'sleep 0.5',
-            f'xdotool key --delay 10 Tab Tab Tab Tab Tab Tab {direction} Tab Return',
+        direction = "Left" if name == "light" else "Right"
+        cli.run_commands(
+            "jumpapp chromium",
+            "sleep 1",
+            "xdotool key ctrl+t",
+            "xdotool type chrome://flags",
+            "xdotool key Return",
+            "sleep 0.5",
+            "xdotool type dark",
+            "xdotool key Return",
+            "sleep 0.5",
+            f"xdotool key --delay 10 Tab Tab Tab Tab Tab Tab {direction} Tab Return",
         )
--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-NAME = 'colortheme'
+NAME = "colortheme"
+
 
 def read(filename):
     try:
         with open(filename) as fp:
-            content = fp.read().split('\n')
+            content = fp.read().split("\n")
     except FileNotFoundError:
         content = []
     return content
 
+
 setup(
-    author='Quinten Roets',
-    author_email='quinten.roets@gmail.com',
-    description='',
+    author="Quinten Roets",
+    author_email="quinten.roets@gmail.com",
+    description="",
     name=NAME,
-    version='1.0',
+    version="1.0",
     packages=find_packages(),
-    install_requires=read('requirements.txt'),
+    install_requires=read("requirements.txt"),
     entry_points={
-        'console_scripts': [
-            'colortheme = colortheme.main:main',
-            'light = colortheme.main:go_light',
-            'dark = colortheme.main:go_dark',
-            'restartplasma = colortheme.main:restartplasma',
-            ]
-        },
+        "console_scripts": [
+            "colortheme = colortheme.main:main",
+            "light = colortheme.main:go_light",
+            "dark = colortheme.main:go_dark",
+            "restartplasma = colortheme.main:restartplasma",
+        ]
+    },
 )
