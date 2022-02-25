bf4dd15c77683cc95f72ed1ce58352cf094790b0
Quinten <quinten.roets@gmail.com>
2022-02-25 23:23:06 +0100

    adapt main to thememanager module instead of class

--- colortheme/main.py
+++ colortheme/main.py
@@ -1,34 +1,39 @@
 import argparse
 
-from .thememanager import ThemeManager
+from . import thememanager
 
 
 def main(action=None):
-    parser = argparse.ArgumentParser(description='Colortheme actions')
-    parser.add_argument('action', nargs='?', help='The action to do [check[default], dark, light, restart]', default='check')
+    parser = argparse.ArgumentParser(description="Colortheme actions")
+    parser.add_argument(
+        "action",
+        nargs="?",
+        help="The action to do [check[default], dark, light, restart]",
+        default="check",
+    )
     args = parser.parse_args()
     if action is not None:
         args.action = action
-    
-    if args.action == 'check':
-        ThemeManager.start_check_service()
-    elif args.action == 'restart':
-        ThemeManager.restartplasma()
+
+    if args.action == "check":
+        thememanager.start_check_service()
+    elif args.action == "restart":
+        thememanager.restartplasma()
     else:
-        ThemeManager.apply(args.action)
+        thememanager.apply(args.action)
 
 
 def go_dark():
-    main('dark')
+    main("dark")
 
 
 def go_light():
-    main('light')
+    main("light")
 
 
 def restartplasma():
-    main('restart')
+    main("restart")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
