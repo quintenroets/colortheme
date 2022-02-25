1ec7797b045855702c400f98b87cdb12edf8ab12
Quinten <quinten.roets@gmail.com>
2022-02-25 15:42:17 +0100

    remove staticmethods to normal functions

--- colortheme/thememanager.py
+++ colortheme/thememanager.py
@@ -2,104 +2,92 @@ import time
 
 import cli
 import gui
-from backup.profilemanager import ProfileManager
+from backup import profilemanager
 from libs.threading import Threads
 
 from .eventchecker import EventChecker
 
 
-class ThemeManager:
-    @staticmethod
-    def start_check_service():
-        EventChecker(
-            on_light=ThemeManager.on_light, on_dark=ThemeManager.on_dark
-        ).start()
+def start_check_service():
+    EventChecker(
+        on_light=lambda: check_theme("light"), on_dark=lambda: check_theme("dark")
+    ).start()
 
-    @staticmethod
-    def on_light():
-        ThemeManager.check_theme("light")
 
-    @staticmethod
-    def on_dark():
-        ThemeManager.check_theme("dark")
+def check_theme(name):
+    if profilemanager.active_profile.name != name:
+        apply(name, ask_confirm=True)
 
-    @staticmethod
-    def check_theme(name):
-        if ProfileManager.active_profile != name:
-            ThemeManager.apply(name, ask_confirm=True)
 
-    @staticmethod
-    def apply(name, ask_confirm=False):
-        programs = ["pycharm", "dolphin", "kate", "chromium"]
-        open_programs = [
-            p
-            for p in programs
-            if cli.get("xdotool search --onlyvisible", p, check=False)
-        ]
-        confirmed = (
-            not ask_confirm
-            or not any(open_programs)
-            or gui.ask_yn(f"Change to {name} theme?")
-        )
-        if confirmed:
-            ThemeManager.start_apply(name, open_programs)
+def apply(name, ask_confirm=False):
+    programs = ["pycharm", "dolphin", "kate", "chromium"]
+    open_programs = [
+        p for p in programs if cli.get("xdotool search --onlyvisible", p, check=False)
+    ]
+    confirmed = (
+        not ask_confirm
+        or not any(open_programs)
+        or gui.ask_yn(f"Change to {name} theme?")
+    )
+    if confirmed:
+        start_apply(name, open_programs)
 
-        return confirmed
+    return confirmed
 
-    @staticmethod
-    def start_apply(theme, open_programs):
-        custom_apply_mapping = {
-            "chromium": ThemeManager.apply_chromium,
-            "pycharm": ThemeManager.apply_pycharm,
-        }
-        custom_apply = set({})
-        for program, function in custom_apply_mapping.items():
-            if program in open_programs:
-                open_programs.remove(program)
-                custom_apply.add(function)
 
-        ProfileManager.apply(theme)
-        ThemeManager.restartplasma()
-        Threads(ThemeManager.restart, args=(open_programs,)).start().join()
+def start_apply(theme, open_programs):
+    custom_apply_mapping = {
+        "chromium": apply_chromium,
+        "pycharm": apply_pycharm,
+    }
+    custom_apply = set({})
+    for program, function in custom_apply_mapping.items():
+        if program in open_programs:
+            open_programs.remove(program)
+            custom_apply.add(function)
 
-        for custom in custom_apply:
-            time.sleep(2)
-            custom(theme)
+    profilemanager.apply(theme)
+    restartplasma()
+    Threads(restart, args=(open_programs,)).start().join()
 
-    @staticmethod
-    def restart(name):
-        cli.get(f"wmctrl -c {name}")
+    for custom in custom_apply:
+        time.sleep(2)
+        custom(theme)
 
-        # wait until closed
-        while cli.get("xdotool search --onlyvisible", name, check=False):
-            time.sleep(0.5)
 
-        cli.start(name)
+def restart(name):
+    cli.get(f"wmctrl -c {name}")
 
-    @staticmethod
-    def restartplasma():
-        cli.run_commands("plasmashell --replace", "kwin --replace", wait=False)
+    # wait until closed
+    while cli.get("xdotool search --onlyvisible", name, check=False):
+        time.sleep(0.5)
 
-    @staticmethod
-    def apply_pycharm(name):
-        letter = "d" if name == "dark" else "i"
-        cli.run_commands(
-            "jumpapp pycharm",
-            f"xdotool key --clearmodifiers ctrl+shift+alt+y t Return {letter} Return",
-        )
+    cli.start(name)
 
-    @staticmethod
-    def apply_chromium(name):
-        direction = "Left" if name == "light" else "Right"
-        cli.run_commands(
-            "jumpapp chromium",
-            "sleep 1",
-            "xdotool key ctrl+t",
-            "xdotool type chrome://flags",
-            "xdotool key Return",
-            "sleep 0.5",
-            "xdotool type dark",
-            "xdotool key Return",
-            "sleep 0.5",
-            f"xdotool key --delay 10 Tab Tab Tab Tab Tab Tab {direction} Tab Return",
-        )
+
+def restartplasma():
+    cli.run_commands("plasmashell --replace", "kwin --replace", wait=False)
+
+
+def apply_pycharm(name):
+    letter = "d" if name == "dark" else "i"
+    cli.run_commands(
+        "jumpapp pycharm",
+        f"xdotool key --clearmodifiers ctrl+shift+alt+y t Return {letter} Return",
+    )
+
+
+def apply_chromium(name):
+    direction = "Left" if name == "light" else "Right"
+    cli.run_commands(
+        "jumpapp chromium",
+        "sleep 1",
+        "xdotool key ctrl+t",
+        "xdotool type chrome://flags",
+        "xdotool key Return",
+        "sleep 0.5",
+        "xdotool type dark",
+        "xdotool key Return",
+        "sleep 0.5",
+        f"xdotool key --delay 10 Tab Tab Tab Tab Tab Tab {direction} Tab Return",
+    )
