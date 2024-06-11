6a41cac897d58a141ba5b68ece336712c7577080
Quinten <quinten.roets@gmail.com>
2024-06-10 21:29:14 -0700

    update to template structure

--- colortheme/path.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from plib import Path as BasePath
-
-
-class Path(BasePath):
-    root = BasePath(__file__).parent
-    assets = root / "assets"
-    script_templates = assets / "kde_script_templates"
