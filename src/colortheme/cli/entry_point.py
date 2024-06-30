fac8751c29cd91872bd03f338f4a4e7fdc50af5f
Quinten <quinten.roets@gmail.com>
2024-06-30 02:19:04 +0000

    use ruff best practices (#27)

--- src/colortheme/cli/entry_point.py
+++ src/colortheme/cli/entry_point.py
@@ -1,17 +1,6 @@
 from package_utils.context.entry_point import create_entry_point
 
-from colortheme.main import main
-from colortheme.main.thememanager import ThemeManager
+from colortheme import main
+from colortheme.context import context
 
-from ..context import context
-
-entry_point = create_entry_point(main.main, context)
-restart_plasma = ThemeManager.restart_plasma
-
-
-def go_light() -> None:
-    main.apply("light")
-
-
-def go_dark() -> None:
-    main.apply("dark")
+entry_point = create_entry_point(main, context)
--- src/colortheme/main/main.py
+++ src/colortheme/main/main.py
@@ -1,32 +1,13 @@
-import gui
-from backup.backups import profile
+import cli
 
-from .eventchecker import EventChecker
-from .thememanager import ThemeManager
+from colortheme.context import context
 
 
 def main() -> None:
     """
-    Start colortheme check service.
+    Python package template.
     """
-    checker = EventChecker(
-        on_light=lambda: check_theme("light"), on_dark=lambda: check_theme("dark")
-    )
-    checker.start()
-
-
-def check_theme(name: str) -> None:
-    if profile.Backup().profile_name != name:
-        apply(name, ask_confirm=True)
-
-
-def apply(name: str, ask_confirm: bool = False) -> bool:
-    confirmed = (
-        not ask_confirm
-        or not any(ThemeManager.open_programs())
-        or gui.ask_yn(f"Change to {name} theme?")
-    )
-    if confirmed:
-        ThemeManager.start_apply(name)
-
-    return confirmed
+    message = "main functionality"
+    if context.options.debug:
+        cli.console.print(message)
+    cli.console.print(context.secrets)
