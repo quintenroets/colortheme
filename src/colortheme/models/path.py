907042d124c06d14fdf5400aed0cbbf3aa186881
Quinten <quinten.roets@gmail.com>
2025-04-09 03:54:21 +0000

    pre-commit auto updates

--- src/colortheme/models/path.py
+++ src/colortheme/models/path.py
@@ -3,6 +3,7 @@ from typing import TypeVar
 
 import superpathlib
 from simple_classproperty import classproperty
+from typing_extensions import Self
 
 T = TypeVar("T", bound="Path")
 
@@ -10,11 +11,23 @@ T = TypeVar("T", bound="Path")
 class Path(superpathlib.Path):
     @classmethod
     @classproperty
-    def source_root(cls: type[T]) -> T:
+    def source_root(cls) -> Self:
         return cls(__file__).parent.parent
 
     @classmethod
     @classproperty
+<<<<<<< HEAD
     def script_templates(cls: type[T]) -> T:
         path = cls.source_root / "assets" / "script_templates"
         return typing.cast(T, path)
+=======
+    def assets(cls) -> Self:
+        path = cls.script_assets / cls.source_root.name
+        return cast("Self", path)
+
+    @classmethod
+    @classproperty
+    def config(cls) -> Self:
+        path = cls.assets / "config" / "config.yaml"
+        return cast("Self", path)
+>>>>>>> template
