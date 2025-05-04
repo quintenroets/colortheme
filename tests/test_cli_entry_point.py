626548dba72ac40e4538e24921bc78605405b955
Quinten <quinten.roets@gmail.com>
2025-05-03 21:03:45 -0700

    shorter module

--- tests/test_cli_entry_point.py
+++ tests/test_cli_entry_point.py
@@ -5,7 +5,7 @@ from package_dev_utils.tests.args import no_cli_args
 from colortheme import cli
 
 
-@patch("colortheme.main.main.main")
+@patch("colortheme.main.main")
 @no_cli_args
 def test_entry_point(patched_main: MagicMock) -> None:
     cli.entry_point()
