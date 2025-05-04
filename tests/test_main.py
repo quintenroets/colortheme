2c05db99d6af678495e86aae5e9fb531748c69ba
Quinten <quinten.roets@gmail.com>
2025-05-03 21:16:50 -0700

    fix patch

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,11 @@ from unittest.mock import MagicMock, patch
 
 from package_dev_utils.tests.args import no_cli_args
 
-from colortheme import cli
+from colortheme.main.main import main
 
 
 @patch("colortheme.main.eventchecker.EventChecker.start")
 @no_cli_args
 def test_entry_point(patched_start: MagicMock) -> None:
-    cli.entry_point()
+    main()
     patched_start.assert_called_once()
